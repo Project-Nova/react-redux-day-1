32fe45fe7c1eed63cec88a5ea0f3177b1a616f5f
sathish <sathishct2013@gmail.com>
2020-02-29 19:12:24 +0530

    Updates

---
+++ precache-manifest.09a7dc1867e4ae90635ea4367cb3f2b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781870a419b6f5056ca625904cb7d0ad",
+    "url": "//project-nova.github.io/react-redux-day-1/index.html"
+  },
+  {
+    "revision": "fbb25c1306442c822e95",
+    "url": "//project-nova.github.io/react-redux-day-1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "272ff29bd34c4bb6faa5",
+    "url": "//project-nova.github.io/react-redux-day-1/static/js/2.7e624d25.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "//project-nova.github.io/react-redux-day-1/static/js/2.7e624d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb25c1306442c822e95",
+    "url": "//project-nova.github.io/react-redux-day-1/static/js/main.18cd6218.chunk.js"
+  },
+  {
+    "revision": "f1764ff8136432536847",
+    "url": "//project-nova.github.io/react-redux-day-1/static/js/runtime-main.c62f1535.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//project-nova.github.io/react-redux-day-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
